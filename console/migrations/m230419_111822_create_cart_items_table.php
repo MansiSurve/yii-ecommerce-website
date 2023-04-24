89180f6cb3cd749fef354f1152b26ef8da658796
MansiSurve <mansis.marquee@gmail.com>
2023-04-24 13:11:06 +0530

    Generate migration for schema

---
+++ console/migrations/m230419_111822_create_cart_items_table.php
@@ -0,0 +1,63 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%cart_items}}`.
+ */
+class m230419_111822_create_cart_items_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%cart_items}}', [
+            'id' => $this->integer(11)->notNull(),
+            'product_id' => $this->Integer(1)->notNull(),
+            'quantity' => $this->integer(11)->notNull(),
+            'created_by' => $this->integer(11)->notNull(),
+        ]);
+
+        // creates index for column `product_id`
+        $this->createIndex(
+            '{{%idx-cart_items-product_id}}',
+            '{{%cart_items}}',
+            'product_id'
+        );
+
+        // add foreign key for table `{{%product}}`
+        $this->addForeignKey(
+            '{{%fk-cart_items-product_id}}',
+            '{{%cart_items}}',
+            'product_id',
+            '{{%product}}',
+            'id',
+            'CASCADE'
+        );
+
+        $this->createIndex(
+            '{{%idx-cart_items-created_by}}',
+            '{{%cart_items}}',
+            'created_by'
+        );
+
+        // add foreign key for table `{{%user}}`
+        $this->addForeignKey(
+            '{{%fk-cart_items-created_by}}',
+            '{{%cart_items}}',
+            'created_by',
+            '{{%user}}',
+            'id',
+            'CASCADE'
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%cart_items}}');
+    }
+}
